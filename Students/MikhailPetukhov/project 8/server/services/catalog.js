0e3cc1ca5af325f831256f2fe0c66071982c42b6
unknown <petmik@yandex.ru>
2020-05-14 21:02:17 +0300

    VUE-cli done

--- Students/MikhailPetukhov/project 8/server/services/catalog.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	add(item, array) {
-		let newObject = JSON.parse(JSON.stringify(item));
-        newObject.id = Date.now();
-        array.push(newObject);
-        return { newCatalog: array, newId: newObject.id };
-	}
-};
--- Students/MikhailPetukhov/project-8/src/main.js
+++ Students/MikhailPetukhov/project-8/src/main.js
@@ -5,9 +5,13 @@ import './layout/styles/normalize.css';
 import './layout/styles/style.css';
 import './layout/styles/my_style.css';
 
-import Vue from 'vue';
-import app from './app.vue';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
 
 new Vue({
-	render: h => h(app)
-}).$mount('#app');
+  router,
+  render: h => h(App)
+}).$mount('#app')
